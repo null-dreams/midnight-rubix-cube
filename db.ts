b7c7d4dc41d0bf762cfc59156db48ecf7877b973
null-dreams <agneeva0705@gmail.com>
2026-03-28 15:54:04 +0530

    db: create mongoose connector

---
+++ db.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/rubixCube";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(MONGODB_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${(error as Error).message}`);
+    process.exit(1);
+  }
+};
